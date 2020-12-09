689529cc2a6d12edf3c0ad53c6e9e04389d8abd3
Xander Riga <xanderriga@live.com>
2020-12-08 19:13:32 -0800

    Create a dummy homepage
    This gives us some garbage data to see how some things work.

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import Person from './models/person';
+import Gift from './models/gift';
 
 export default function App() {
+  const [people, setPeople] = useState([])
+  const [gifts, setGifts] = useState([])
+
+  const createTables = useCallback(async () => {
+    await Person.createTable()
+    await Gift.createTable()
+  }, [])
+
+  const createPeople = useCallback(async () => {
+    const kendra = new Person({ name: 'Kendra' });
+    const collin = new Person({ name: 'Collin' });
+
+    await kendra.save();
+    await collin.save();
+    setPeople(await Person.query())
+  }, [])
+
+  const createGifts = useCallback(async () => {
+    const gift = new Gift({ name: 'socks', person_id: 1 });
+
+    await gift.save();
+
+    setGifts(await Gift.query());
+  })
+
+  React.useEffect(() => {
+    createTables();
+    createPeople();
+    createGifts();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView style={{ flex: 1 }}>
+        {
+          people.map(person => <Text key={person.id}>{person.name}</Text>)
+        }
+      </ScrollView>
+
+      <ScrollView style={{ flex: 1 }}>
+        {
+          gifts.map(gift => <Text key={gift.id}>{gift.name}</Text>)
+        }
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
