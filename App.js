e4512ff1a69843318b97bac33801038c28e6a80a
Xander Riga <xanderriga@live.com>
2020-12-08 19:34:29 -0800

    Add navigation to app

--- App.js
+++ App.js
@@ -3,57 +3,32 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useCallback, useState } from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import Person from './models/person';
-import Gift from './models/gift';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from './components/home_screen';
 
 export default function App() {
   const [people, setPeople] = useState([])
-  const [gifts, setGifts] = useState([])
 
-  const createTables = useCallback(async () => {
-    await Person.createTable()
-    await Gift.createTable()
-  }, [])
-
-  const createPeople = useCallback(async () => {
-    const kendra = new Person({ name: 'Kendra' });
-    const collin = new Person({ name: 'Collin' });
-
-    await kendra.save();
-    await collin.save();
+  const loadPeople = useCallback(async () => {
     setPeople(await Person.query())
-  }, [])
-
-  const createGifts = useCallback(async () => {
-    const gift = new Gift({ name: 'socks', person_id: 1 });
-
-    await gift.save();
-
-    setGifts(await Gift.query());
   })
 
+  const Stack = createStackNavigator();
+
   React.useEffect(() => {
-    createTables();
-    createPeople();
-    createGifts();
+    loadPeople();
   }, [])
 
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <ScrollView style={{ flex: 1 }}>
-          {
-            people.map(person => <Text key={person.id}>{person.name}</Text>)
-          }
-        </ScrollView>
-
-        <ScrollView style={{ flex: 1 }}>
-          {
-            gifts.map(gift => <Text key={gift.id}>{gift.name}</Text>)
-          }
-        </ScrollView>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
