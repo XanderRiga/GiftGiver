e876d7fd80d9d87b434bae240010a6aba49a05e1
Xander Riga <xanderriga@live.com>
2020-12-13 19:03:28 -0800

    Add price and notes to gift form

--- components/giftForm.js
+++ components/giftForm.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import {StyleSheet} from "react-native";
-import {Button, Container, Content, Form, Item, Input, Text, Toast} from 'native-base';
+import {Button, Container, Content, Form, Item, Icon, Input, Text, Textarea, Toast} from 'native-base';
 import Gift from "../models/gift";
 
 export function GiftForm({navigation, route}) {
   const [name, setName] = useState('')
+  const [notes, setNotes] = useState('')
+  const [price, setPrice] = useState('0')
 
   const submitGift = async () => {
     if (!name) {
@@ -15,20 +17,51 @@ export function GiftForm({navigation, route}) {
       return;
     }
 
-    const gift = new Gift({name: name, person_id: route.params.person.id})
+    const price_cents = buildPrice(price)
+    if (isNaN(price_cents)) {
+      Toast.show({
+        text: "Price must be a number",
+        buttonText: 'Ok'
+      });
+      return;
+    }
+
+    const gift = new Gift({
+      name: name,
+      person_id: route.params.person.id,
+      notes: notes,
+      price_cents: price_cents
+    })
     await gift.save();
     navigation.goBack();
+  }
+
+  const buildPrice = () => {
+    const priceFloat = parseFloat(price)
+    return Math.round( priceFloat * 100);
   }
 
   return (
     <Container>
       <Content padder>
-        <Form>
-          <Item>
+        <Form style={styles.form}>
+          <Item style={styles.input}>
             <Input
               onChangeText={val => setName(val)}
               placeholder="Name" />
           </Item>
+          <Item style={styles.input}>
+            <Icon active name='dollar-sign' type={'FontAwesome5'} />
+            <Input
+                onChangeText={val => setPrice(val)}
+                placeholder="Price" />
+          </Item>
+          <Textarea
+              rowSpan={5}
+              bordered
+              placeholder="Notes"
+              style={styles.textArea}
+              onChangeText={val => setNotes(val)}/>
           <Button
             onPress={submitGift}
             success
@@ -41,8 +74,19 @@ export function GiftForm({navigation, route}) {
     </Container>
   );
 }
+
 const styles = StyleSheet.create({
   saveButton: {
     marginTop: 25
+  },
+  input: {
+    marginTop: 15,
+    marginBottom: 15
+  },
+  textArea: {
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 25,
+    marginBottom: 25,
   }
 });
