454809ca15daac20442cb14c79a271c7b478d943
Xander Riga <xanderriga@live.com>
2020-12-13 22:24:44 -0800

    Add divider for checked and unchecked items

--- components/giftList.js
+++ components/giftList.js
@@ -1,5 +1,5 @@
 import React, {useCallback, useState} from 'react';
-import {Body, CheckBox, Container, Content, List, ListItem, Text} from 'native-base';
+import {Body, CheckBox, Container, Content, List, ListItem, Separator, Right, Icon, Text} from 'native-base';
 import Gift from '../models/gift';
 import {StyleSheet} from "react-native";
 import {truncate} from "../helpers/truncate";
@@ -8,7 +8,8 @@ export function GiftList({navigation, route}) {
   const [gifts, setGifts] = useState([]);
 
   const loadGifts = useCallback(async () => {
-    setGifts(await Gift.query({where: {person_id_eq: route.params.person.id}}));
+    const loadedGifts = await Gift.query({where: {person_id_eq: route.params.person.id}})
+    setGifts(loadedGifts);
   }, []);
 
   React.useEffect(() => {
@@ -26,19 +27,53 @@ export function GiftList({navigation, route}) {
     await loadGifts();
   }
 
+  const containsCheckedGifts = () => {
+    gifts.forEach(gift => {
+      if (gift.checked) {
+        console.log(gift)
+        return true;
+      }
+    })
+
+    return false;
+  }
+
   return (
     <Container>
       <Content>
         <List>
           {
             gifts.map(gift =>
-              <ListItem key={gift.id} onPress={() => clickGift(gift)}>
+              !gift.checked ? <ListItem key={gift.id} onPress={() => clickGift(gift)}>
                 <CheckBox checked={gift.checked} onPress={() => toggleGiftChecked(gift)} />
                 <Body>
                   <Text>{gift.name}</Text>
-                  {gift.notes? <Text style={styles.subText}>{truncate(gift.notes, 60)}</Text> : null}
+                  {gift.notes? <Text style={styles.subText}>{truncate(gift.notes, 50)}</Text> : null}
                 </Body>
-              </ListItem>)
+                <Right>
+                  <Icon active name="arrow-forward" />
+                </Right>
+              </ListItem> : undefined)
+          }
+
+          {
+            containsCheckedGifts ? <Separator /> : undefined
+          }
+
+          {
+            gifts.map(gift =>
+              gift.checked ?
+                <ListItem key={gift.id} onPress={() => clickGift(gift)}>
+                  <CheckBox checked={gift.checked} onPress={() => toggleGiftChecked(gift)} />
+                  <Body>
+                    <Text>{gift.name}</Text>
+                    {gift.notes? <Text style={styles.subText}>{truncate(gift.notes, 50)}</Text> : null}
+                  </Body>
+                  <Right>
+                    <Icon active name="arrow-forward" />
+                  </Right>
+                </ListItem> : undefined
+            )
           }
         </List>
       </Content>
